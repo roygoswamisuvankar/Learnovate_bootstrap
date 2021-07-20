7e56586138a1507458998bd2c65fee84a04bd267
suvankar roy goswami <46588797+roygoswamisuvankar@users.noreply.github.com>
2021-07-20 12:26:18 +0530

    Learnovate assignment using bootstarp grid

---
+++ script/script.js
@@ -0,0 +1,125 @@
+//1st image and 1st view button
+$(document).ready(function(){
+    $("#button1").hide();
+    $("#img1").hover(function(){
+        $("#button1").show();
+        $("#button1").hover(function(){
+            $("#button1").show();
+            $("#img1").css({
+                'opacity' : '0.7',
+            })
+        }, function(){
+            $("#button1").hide();
+            $("#img1").css({
+                'opacity':'10',
+            })
+        })
+    }, function(){
+        $("#button1").hide();
+    });
+});
+//2nd image and 2nd view button
+$(document).ready(function(){
+    $("#button2").hide();
+    $("#img2").hover(function(){
+        $("#button2").show();
+        $("#button2").hover(function(){
+            $("#button2").show();
+            $("#img2").css({
+                'opacity' : '0.7',
+            })
+        }, function(){
+            $("#button2").hide();
+            $("#img2").css({
+                'opacity':'10',
+            })
+        })
+    }, function(){
+        $("#button2").hide();
+    });
+});
+
+//3rd image and view button
+$(document).ready(function(){
+    $("#button3").hide();
+    $("#img3").hover(function(){
+        $("#button3").show();
+        $("#button3").hover(function(){
+            $("#button3").show();
+            $("#img3").css({
+                'opacity' : '0.7',
+            })
+        }, function(){
+            $("#button3").hide();
+            $("#img3").css({
+                'opacity':'10',
+            })
+        })
+    }, function(){
+        $("#button3").hide();
+    });
+});
+
+//4th image and 4th view button
+$(document).ready(function(){
+    $("#button4").hide();
+    $("#img4").hover(function(){
+        $("#button4").show();
+        $("#button4").hover(function(){
+            $("#button4").show();
+            $("#img4").css({
+                'opacity' : '0.7',
+            })
+        }, function(){
+            $("#button4").hide();
+            $("#img4").css({
+                'opacity':'10',
+            })
+        })
+    }, function(){
+        $("#button4").hide();
+    });
+});
+
+//5th image and 5th view button
+
+$(document).ready(function(){
+    $("#button5").hide();
+    $("#img5").hover(function(){
+        $("#button5").show();
+        $("#button5").hover(function(){
+            $("#button5").show();
+            $("#img5").css({
+                'opacity' : '0.7',
+            })
+        }, function(){
+            $("#button5").hide();
+            $("#img5").css({
+                'opacity':'10',
+            })
+        })
+    }, function(){
+        $("#button5").hide();
+    });
+});
+
+//6th image and view buttons
+$(document).ready(function(){
+    $("#button6").hide();
+    $("#img6").hover(function(){
+        $("#button6").show();
+        $("#button6").hover(function(){
+            $("#button6").show();
+            $("#img6").css({
+                'opacity' : '0.7',
+            })
+        }, function(){
+            $("#button6").hide();
+            $("#img6").css({
+                'opacity':'10',
+            })
+        })
+    }, function(){
+        $("#button6").hide();
+    });
+});
